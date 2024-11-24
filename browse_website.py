a021f7952f28e81c67a797c4b2e8c9586d96fa6c
Manan Gandhi <ardumanan@gmail.com>
2024-11-24 16:24:52 +0530

    Fix errors

--- browse_website.py
+++ browse_website.py
@@ -5,7 +5,6 @@ from langchain.agents import tool
 from langchain.agents import AgentExecutor, create_react_agent
 from langchain_core.messages import AIMessage, HumanMessage
 from dotenv import load_dotenv
-from langchain.agents.output_parsers.openai_tools import OpenAIToolsAgentOutputParser
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 
 
@@ -40,7 +39,7 @@ prompt = ChatPromptTemplate.from_messages(
         (
             "system",
             """Answer the following questions as best you can. You have access to the following tools:
-
+YOU HAVE INTERNET ACCESS SO YOU CAN USE ANY TOOL THAT REQUIRES INTERNET ACCESS
 {tools}
 
 Use the following format:
@@ -64,8 +63,8 @@ Final Answer: the final answer to the original input question
 Begin!""",
         ),
         MessagesPlaceholder(variable_name="chat_history"),
-        ("user", "{input}"),
-        ("ai", "{agent_scratchpad}"),
+        ("user", "User: {input}"),
+        ("ai", "Scratchpad: {agent_scratchpad}"),
     ]
 )
 llm_with_tools = llm.bind_tools(tools)
@@ -91,7 +90,6 @@ while True:
         {
             "input": message,
             "chat_history": chat_history,
-            "agent_scratchpad": agent_scratchpad,
         }
     )
     print(result["output"])
